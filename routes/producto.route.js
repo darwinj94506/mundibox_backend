822f923cc31cb4308ebb3604880cb857e4accc4e
darwinj94 <darwinjair18@gmail.com>
2019-04-02 14:02:25 -0500

    listo para subir imagenes

--- routes/producto.route.js
+++ routes/producto.route.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var ProductoController=require('./../controllers/producto.controller');
-var buscar_upcController=require('./../controllers/bucar_upc.controller');
+var ProductoController = require('./../controllers/producto.controller');
+var buscar_upcController = require('./../controllers/bucar_upc.controller');
+
+var multipart = require('connect-multiparty');
+var carpeta_destino_imagen = multipart({ uploadDir: './public/images/productos' });
 
 // router.post('/getListaTipos', MaterialController.getListaTipos);
-router.post('/getMaterialesSelect2',ProductoController.getMaterialesSelect2);
-router.post('/findProductos',ProductoController.findProductos);
-router.post('/crudProducto',ProductoController.crudProducto);
-router.get('/findProductoByCodigoFabricante/:codigofabricante',ProductoController.findProductoByCodigoFabricante);
+router.post('/getMaterialesSelect2', ProductoController.getMaterialesSelect2);
+router.post('/findProductos', ProductoController.findProductos);
+router.post('/crudProducto', ProductoController.crudProducto);
+router.get('/findProductoByCodigoFabricante/:codigofabricante', ProductoController.findProductoByCodigoFabricante);
 // router.post('/findProductosByImportacion',ProductoController.findProductosByImportacion);
-router.post('/buscarupc',buscar_upcController.buscarUPC);
+router.post('/buscarupc', buscar_upcController.buscarUPC);
 module.exports = router;
