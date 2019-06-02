1def7793016713445ff22700a0322c1464c23d1e
darwinj94 <darwinj94@hotmail.com>
2019-06-01 23:37:14 -0500

    cambio de conecction string

--- bdd.coneccion.js
+++ bdd.coneccion.js
@@ -6,21 +6,41 @@ var options = {
         // A connection-related error;
         console.log("CN:", e.cn);
         console.log("EVENT:", error.message);
+        console.log(error)
     }
 }
 };
+
+
+/*
+
 const connectionString = {
-  host: '192.168.3.182',
+  host: 'https://node45590-env-0992479.jl.serv.net.mx',
   port: 5432,
   database: 'mundiboxdb_v2',
   user: 'webadmin',
-  password: '35v6x9Qds2'
+  password: 'RAMydg13977'
 };
 
-var pgp = require('pg-promise')(options);
-// var URL =  "postgres://webadmin:35v6x9Qds2@192.168.3.182:5432/mundiboxdb_v2";
+const connectionString = {
+  host: 'node45590-env-0992479.jl.serv.net.mx',
+  port: 5432,
+  database: 'mundiboxdb_v2',
+  user: 'webadmin',
+  password: 'RAMydg13977'
+}; 
+var connectionString =  "postgres://webadmin:RAMydg13977@https://node45590-env-0992479.jl.serv.net.mx:5432/mundiboxdb_v2";
+var connectionString =  "postgres://webadmin:RAMydg13977@node45590-env-0992479.jl.serv.net.mx:5432/mundiboxdb_v2";
+var connectionString =  "postgres://webadmin:RAMydg13977@node45590-env-0992479.jl.serv.net.mx/mundiboxdb_v2";
 
+LA QUE SI VALE LOCALMENTE
 const connectionString = "postgres://postgres:root@localhost:5432/mundiboxdb_v2";
+*/ 
+
+var pgp = require('pg-promise')(options);
+
+var connectionString =  "postgres://postgres:root@node45590-env-0992479.jl.serv.net.mx:5432/mundiboxdb_v2";
+
 
 
 
